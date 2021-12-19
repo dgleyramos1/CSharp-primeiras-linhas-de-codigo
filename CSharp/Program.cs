daaafa7870d26b0945b7bbbab6111539ad511922
dgley <dgley@DGLEY-PC>
2021-12-18 23:24:43 -0300

    Add project files.

---
+++ CSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CSharp;
+using Interface;
+
+namespace CSharp
+{
+   class Progam
+    {
+        public static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "Dgley Ramos";
+            pessoa.Idade = 27;
+            pessoa.Estado = "Paraiba";
+
+            Console.WriteLine("Nome: " + pessoa.Nome + "\n" + "Idade: " + pessoa.Idade + "\n" + "Estado: " + pessoa.Estado + "\n");
+
+            Animal animal = new Animal();
+            animal.Nome = "Jubileu";
+            animal.NomeDono = "Ramos";
+            animal.Especie = "Caramelo";
+
+            Console.WriteLine("Nome:" + animal.Nome + "\n" + "Nome do Dono:" + animal.NomeDono + "\n" + "Especie:" + animal.Especie + "\n");
+
+        }
+    }
+}
